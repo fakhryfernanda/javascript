675139f83fade4ed28287c6a35272b0f54f64702
Fakhry Fernanda <mff023@gmail.com>
2022-03-23 08:14:30 +0700

    Terjawab

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let preValue = 1;
+    let curValue = 1;
+    let temp;
+    for (let i = 3; i <= n; i++) {
+        temp = curValue;
+        curValue = preValue + curValue;
+        preValue = temp;
+    }
+    return curValue;
 };
 
 // Do not edit below this line
