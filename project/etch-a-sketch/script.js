1abbd6f977965c8eaf1cb948a98c6d35d59b24e8
Fakhry Fernanda <mff023@gmail.com>
2022-03-23 07:28:49 +0700

    Bug: ada gap vertikal di grid setelah tombol clear ditekan

--- project/etch-a-sketch/script.js
+++ project/etch-a-sketch/script.js
@@ -3,15 +3,15 @@
 // containerSize = 16 * gridSize + 15 * gap + 32 * 1;
 // gridSize = 16;
 const containerSize = 800;
-const gap = 1;
+const gap = 0;
 const border = 0;
-let numGrids = 16;
-let gridSize = (containerSize - (numGrids-1) * gap - (numGrids * 2) * border) / numGrids;
+let numGrids = 32;
+let gridSize = (containerSize - (numGrids - 1) * gap - (numGrids * 2) * border) / numGrids;
 
 const container = document.querySelector('.container');
 container.style.width = `${containerSize}px`;
 container.style.height = `${containerSize}px`;
-container.style.gap = `${gap}px`
+container.style.gap = `${gap}px ${gap}px`;
 
 const createGrids = (numGrids) => {
     for (let i = 0; i < numGrids * numGrids; i++) {
@@ -26,16 +26,19 @@ const createGrids = (numGrids) => {
 createGrids(numGrids);
 
 // Change background color when hovering
-const hoveringFunction = () => {
+// let color = 'black';
+const hoveringFunction = (color) => {
     const grids = document.querySelectorAll('.grid');
     grids.forEach(grid => {
         grid.addEventListener('mouseover', e => {
-            e.target.style.backgroundColor = 'red';
+            if (color !== 'random') {
+                e.target.style.backgroundColor = color;
+            };
         });
     });
 }
 
-hoveringFunction();
+hoveringFunction('black');
 
 // Clear button
 const clearButton = document.querySelector('.clear');
@@ -46,6 +49,5 @@ clearButton.addEventListener('click', () => {
     gridSize = (containerSize - (numGrids-1) * gap - (numGrids * 2) * 1) / numGrids;
     container.innerHTML = "" // Delete previous grids
     createGrids(numGrids);
-    hoveringFunction();
-    // grids.forEach(grid => grid.style.backgroundColor = 'white');
+    hoveringFunction('red');
 });
